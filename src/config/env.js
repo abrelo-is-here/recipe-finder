33f137d84fb0b78bb7b8fc94e3c84e0a5744d0c9
abrelo-is-here <159897417+abrelo-is-here@users.noreply.github.com>
2025-09-14 07:41:40 +0300

    Update env.js
    node status updated from development to production

--- src/config/env.js
+++ src/config/env.js
@@ -3,6 +3,6 @@ import "dotenv/config"
 export const ENV = {
     PORT: process.env.PORT,
     DB_URL: process.env.DB_URL, 
-    NODE_ENV: process.env.NODE_ENV,
+    NODE_ENV: "production",
     API_URL: process.env.API_URL
 }
